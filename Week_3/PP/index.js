2c91a3e657a10fd1ecddfe7152bd454c879e1d3e
tyrawalker <tyra.walker@bryanuniversity.edu>
2020-12-05 17:19:58 -0500

    week3 done

---
+++ Week_3/PP/index.js
@@ -0,0 +1,12 @@
+for (var i = 0; i <5; i++){
+    var header = document.createElement("h2")
+    header.innerHTML = "Java Script is NOT fun"
+    header.style.fontSize= "20px"
+    header.style.fontWeight= "lighter"
+    header.style.fontFamily= "sans-serif"
+    header.style.color= "cornflowerblue"
+    document.body.append (header)
+
+}
+ 
+document.querySelector ("body").classList.add ("border").innerHTML

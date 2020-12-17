1b6968c167b1c53865debf5bdce2f772b728be15
tyrawalker <tyra.walker@bryanuniversity.edu>
2020-12-16 20:40:05 -0500

    week_4_pp

---
+++ Week_4/index.js
@@ -0,0 +1,26 @@
+var square = document.createElement("div")
+square.textContent = "Im a box"
+square.style.textAlign = "center"
+document.body.prepend (square)
+
+document.querySelector ("div").classList.add ("border").innerHTML
+
+square.addEventListener("mouseover",function() {
+    square.style.backgroundColor = "#0B25E3";
+})
+
+square.addEventListener("mousedown", function () {
+    square.style.backgroundColor = "#E30B2C";
+})
+
+square.addEventListener ("mouseup", function () {
+    square.style.backgroundColor= "#E3DF0B";
+})
+
+square.addEventListener ("dblclick", function () {
+    square.style.backgroundColor = "#05AB31"; 
+})
+
+document.body.addEventListener ("scroll", function () {
+    document.body.style.backgroundColor = "#F29807";
+})

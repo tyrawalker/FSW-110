f8880787ab4289e60557f5a6308c7359c7b90a86
tyrawalker <tyra.walker@bryanuniversity.edu>
2020-12-29 11:14:42 -0500

    Calc final

--- Week_4/calculator/calc.js
+++ Week_4/calculator/calc.js
@@ -1,10 +1,27 @@
 
 
 document.addEventListener('DOMContentLoaded', init, false)
+
 document.getElementById("add").addEventListener ("click", function () {
     var result1 = document.createElement("div")
     result1.textContent = function(num1, num2){
-    num1 + num2
+    document.getElementById("num1") + document.getElementById("num2")
 }
 document.getElementById ("addition").append(result1)
+})
+
+document.getElementById("sub").addEventListener ("click", function () {
+    var result2 = document.createElement("div")
+    result2.textContent = function(num3, num4){
+    document.getElementById("num3") - document.getElementById("num4")
+}
+document.getElementById ("subtraction").append(result2)
+})
+
+document.getElementById("mul").addEventListener ("click", function () {
+    var result3 = document.createElement("div")
+    result3.textContent = function(num5, num6){
+    document.getElementById("num5") * document.getElementById("num6")
+}
+document.getElementById ("mulitply").append(result3)
 })

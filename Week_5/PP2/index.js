8cb1046723e4ce27314812a846bc5870b6f0343d
tyrawalker <tyra.walker@bryanuniversity.edu>
2020-12-30 10:45:40 -0500

    week 5 pp redo

---
+++ Week_5/PP2/index.js
@@ -0,0 +1,11 @@
+var fname = document.travelerInfo.fname.value; 
+var lname = document.travelerInfo.lname.value; 
+var age = document.travelerInfo.age.value; 
+var location = document.travelerInfo.age.value; 
+var diet = document.travelerInfo.diet.value; 
+
+
+    alert (fname + lname + age + location + diet);
+} )
+ 
+
--- Week_5/index.js
+++ Week_5/index.js
@@ -1,4 +1,6 @@
 const click = document.getElementById ("subscribe").addEventListener ("click", function(){
-  console.log (document.getElementById("fname")) ;
-  ;
+  console.log () ;
+  document.getElementById("fname");
+  document.getElementById("lname")
+  document.getElementById("email");
 })
